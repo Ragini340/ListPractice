3c5d3d65fbc65ca487ca3dd1e61d3c6ed2ef25cf
Ragini340 <ragini31bxr@gmail.com>
2022-12-02 17:43:42 +0530

    Add project files.

---
+++ AnyMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListPractice
+{
+    class AnyMethod
+    {
+        static void Main()
+        {
+            int[] arr = { 5, 7, 10, 12, 15, 18, 20 };
+
+            // Here hecking if any of the array elements are greater than 10
+            bool res = arr.AsQueryable().Any(val => val > 10);
+            Console.WriteLine(res);
+        }
+    }
+}
+
---
+++ AsQueryableMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListPractice
+{
+    class AsQueryableMethod
+    {
+        static void Main()
+        {
+            var arr = new int[] { 100, 200, 300, 400 };
+            int res = Queryable.Sum(arr.AsQueryable());
+            Console.WriteLine("Sum will be : " + res);
+        }
+    }
+}
